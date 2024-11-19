d4f4a1bb90e5c4ac79ff8e4fc9f427f56f345407
mabrursatori <mabrursatori@gmail.com>
2024-11-19 15:17:43 +0700

    Getting Started with Jasper

--- src/main/java/co/id/mabrur/App.java
+++ src/main/java/co/id/mabrur/App.java
@@ -1,5 +1,15 @@
 package co.id.mabrur;
 
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.base.JRBaseTextField;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello world!
  *
@@ -8,6 +18,32 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        try{
+            String filePath ="D:\\java_app\\jasperdemo\\src\\main\\resources\\SimpleReport.jrxml";
+
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("studentName", "John");
+
+            Student student1 = new Student(1L, "Roni", "Din", "Fatahillah Street", "Cirebon");
+            Student student2 = new Student(2L, "Suki", "Puki", "Dewi Sartike Street", "Tegal");
+            List<Student> list = new ArrayList<>();
+            list.add(student1);
+            list.add(student2);
+
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(list);
+
+            JasperReport report = JasperCompileManager.compileReport(filePath);
+
+            JRBaseTextField textField = (JRBaseTextField) report.getTitle().getElementByKey("name");
+
+            textField.setForecolor(Color.RED);
+
+            JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource);
+
+            JasperExportManager.exportReportToPdfFile(print, "D:\\java_app\\ExampleReport.pdf");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
